8efbd17b8d4156b7f8518552422944260079608c
Roman Ness <roman.ness@cosee.biz>
2018-08-23 18:29:36 +0200

    update install script

--- install.sh
+++ install.sh
@@ -6,7 +6,5 @@ RELEASE=${RELEASE:-$(curl --silent https://api.github.com/repos/cosee-gitlab/loc
 
 echo "Installing gitlab-lock to ~/.local/bin in version ${RELEASE} and LOCK_ARCH=${LOCK_ARCH}"
 
-cd $(mktemp -d)
-curl --silent -L "https://github.com/cosee-gitlab/lock/releases/download/v${RELEASE}/lock_${RELEASE}_linux_${LOCK_ARCH}.tar.gz" | tar -xz lock
-
-mv lock ~/.local/bin
+curl --silent -L "https://github.com/cosee-gitlab/lock/releases/download/v${RELEASE}/lock_${RELEASE}_linux_${LOCK_ARCH}.tar.gz" \
+        | tar -xz -C ~/.local/bin/ lock
