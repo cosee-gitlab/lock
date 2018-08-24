2943c64d3df83b9acfb422165c3fc5828ce22319
Roman Ness <roman.ness@cosee.biz>
2018-08-24 15:06:25 +0200

    rename binary to glock to avoid conflicts with other tools

--- install.sh
+++ install.sh
@@ -7,12 +7,12 @@ LATEST_RELEASE=$(curl --silent https://api.github.com/repos/cosee-gitlab/lock/re
         | sed -E 's/.*"v([^"]+)".*/\1/' )   # use sed instead of grep -P, since that might be unavailable
 RELEASE=${RELEASE:-${LATEST_RELEASE}}
 
-echo "Installing gitlab-lock to ~/.local/bin in version ${RELEASE} and LOCK_ARCH=${LOCK_ARCH}"
+echo "Installing gitlab lock (glock) to PATH in version ${RELEASE} and LOCK_ARCH=${LOCK_ARCH}"
 
 old_path=$(pwd)
 cd $(mktemp -d)
 curl --silent -L "https://github.com/cosee-gitlab/lock/releases/download/v${RELEASE}/lock_${RELEASE}_linux_${LOCK_ARCH}.tar.gz" \
-        | tar -xz lock
+        | tar -xz glock
 
 export PATH="$PATH:$(pwd)"
 cd ${old_path}
