3747f7febb08d68aaae424838f5d236e6f63e576
Roman Ness <roman.ness@cosee.biz>
2018-08-24 11:54:07 +0200

    jump back to previous directory in install script

--- install.sh
+++ install.sh
@@ -9,8 +9,10 @@ RELEASE=${RELEASE:-${LATEST_RELEASE}}
 
 echo "Installing gitlab-lock to ~/.local/bin in version ${RELEASE} and LOCK_ARCH=${LOCK_ARCH}"
 
+old_path=$(pwd)
 cd $(mktemp -d)
 curl --silent -L "https://github.com/cosee-gitlab/lock/releases/download/v${RELEASE}/lock_${RELEASE}_linux_${LOCK_ARCH}.tar.gz" \
         | tar -xz lock
 
 export PATH="$PATH:$(pwd)"
+cd ${old_path}
